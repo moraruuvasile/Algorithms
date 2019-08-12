e781ec584981c4c6c7cb4460972fa3562b08d321
moraruuvasile <vaseoa@yahoo.com>
2019-08-12 18:50:32 +0100

    final

--- lib/src/main/java/com/example/lib/My.java
+++ lib/src/main/java/com/example/lib/My.java
@@ -2,15 +2,14 @@ package com.example.lib;
 
 public class My {
 	public static void main(String[] args) {
-		System.out.println(coinChange(12, new int[]{1,5,10}));
+		System.out.println(coinChange(6, new int[]{1,2,5}));
 	}
 
 	private static int coinChange(int numar, int[] monede){
 		int[] optiuni = new int[numar + 1];
 		optiuni[0] = 1;
-		int suma;
 		for(int monedaTip:monede){
-			suma =0;
+			int suma =0;
 			while(suma++ < optiuni.length-1){
 				if(monedaTip <= suma){
 					optiuni[suma]+= optiuni[suma-monedaTip];
